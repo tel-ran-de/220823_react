36611b455a7db4bd06bd6944ad0b47adf690b811
callbackKid <vladislav.yarosh23@gmail.com>
2024-02-26 17:29:54 +0400

    useState

--- src/components/AddNewComment/AddNewComment.jsx
+++ src/components/AddNewComment/AddNewComment.jsx
@@ -2,13 +2,40 @@ import { useState } from 'react'
 
 const AddNewComment = () => {
   const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('')
+
+  // при сабмите формы значения body author выводятся в консоль
+  // onSubmit
+  const handleSubmit = (event) => {
+    // форма перезагружает страницу
+    event.preventDefault()
+    console.log(body, author)
+    // значение body и author сделать пустыми строками и
+    // через функцию изменяющую состояние
+    setBody('')
+    setAuthor('')
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="body">
         Your comment
-        <input type="text" id="body" onChange={(event) => setBody(event.target.value)} />
+        <input
+          type="text"
+          id="body"
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        />
       </label>
-      {body}
+      <label htmlFor="author">
+        Author
+        <input
+          type="text"
+          id="author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        />
+      </label>
+      <button type="submit">submit</button>
     </form>
   )
 }
--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react'
+
 const Comment = ({ author, body, likes }) => {
+  const [marked, setMarked] = useState(true)
+  const [bgMarked, setbgMarked] = useState(false)
+  // bgMarked
+  // импортировать фук useState
+  // деструктурировать его marked setMarked
+  // initState false
+  // в зависимости от marked
+  // style - color: marked ?  orange  : green
+  // создать кнопку которая меняет состояние на противоположное
+
+  // создать новую и новое состояние и менять значение заднего фона
+  // красным либо фиолетовым
   return (
-    <div>
+    <div
+      style={{ color: marked ? 'orange' : 'green', backgroundColor: bgMarked ? 'red' : 'violet' }}
+    >
       <h1>{author}</h1>
       <p>{body}</p>
       <p>{likes}</p>
+      <button onClick={() => setMarked(!marked)}>change color</button>
+      <button onClick={() => setbgMarked(!bgMarked)}>change background</button>
     </div>
   )
 }
